4d705bf6b270554f4be909096fb4d48af1037bb4
Mischa Taylor <mischa@misheska.com>
2014-05-10 22:38:58 -0700

    Working test-box.sh & ssh-box.sh

--- bin/ssh-box.sh
+++ bin/ssh-box.sh
@@ -1,21 +1,8 @@
 #!/bin/bash -eux
 
-create_vagrantfile_linux() {
-cat << EOF > $tmp_path/Vagrantfile
-Vagrant.configure('2') do |config|
-config.vm.box = '$box_name'
-
-config.vm.provision :serverspec do |spec|
-spec.pattern = '$test_src_path'
-end
-end
-EOF
-}
-
 box_path=$1
 box_provider=$2
 vagrant_provider=$3
-test_src_path=$4
 
 box_filename=$(basename "${box_path}")
 box_name=${box_filename%.*}
@@ -23,14 +10,14 @@ tmp_path=/tmp/boxtest
 
 rm -rf ${tmp_path}
 
-vagrant box remove ${box_name} --provider ${box_provider} 2>/dev/null || true
+vagrant box remove ${box_name} --provider ${box_provider} || true
 vagrant box add ${box_name} ${box_path}
 mkdir -p ${tmp_path}
 
 pushd ${tmp_path}
-create_vagrantfile_linux
+vagrant init ${box_name}
 VAGRANT_LOG=warn vagrant up --provider ${vagrant_provider}
-sleep 10
+vagrant ssh
 vagrant destroy -f
 popd
 
