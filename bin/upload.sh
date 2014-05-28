7fdf9448e0cb94d733eb25f078373320e2b66628
Mischa Taylor <misheska@getchef.com>
2014-05-28 14:34:09 -0700

    Fixing upload bit

--- bin/upload.sh
+++ bin/upload.sh
@@ -17,21 +17,21 @@ VBOX_GUEST_ADDITIONS_VERSION="${VBOX_VERSION%%r*}"
 VMWARE_TOOLS_VERSION="9.6.2"
 
 S3_GRANTS="--grants full=id=${S3_CANONICAL_ID} read=uri=http://acs.amazonaws.com/groups/global/AllUsers"
-S3_BUCKET_PATH_VMWARE="s3://box-cutter-us-east-1-cloudtrail/vmware${VMWARE_TOOLS_VERSION}/"
+S3_BUCKET_PATH_VMWARE="s3://box-cutter-us-east-1-cloudtrail/windows/vmware${VMWARE_TOOLS_VERSION}/"
 cmd="aws s3 sync ${DIR}/box/vmware/ ${S3_BUCKET_PATH_VMWARE} ${S3_GRANTS}"
 echo ${cmd}
 ${cmd}
-S3_BUCKET_PATH_VIRTUALBOX="s3://box-cutter-us-east-1-cloudtrail/virtualbox${VBOX_GUEST_ADDITIONS_VERSION}/"
+S3_BUCKET_PATH_VIRTUALBOX="s3://box-cutter-us-east-1-cloudtrail/windows/virtualbox${VBOX_GUEST_ADDITIONS_VERSION}/"
 cmd="aws s3 sync ${DIR}/box/virtualbox/ ${S3_BUCKET_PATH_VIRTUALBOX} ${S3_GRANTS}"
 echo ${cmd}
 ${cmd}
 
 
-S3_BUCKET_PATH_VMWARE="s3://box-cutter-us-east-2-cloudtrail/vmware${VMWARE_TOOLS_VERSION}/"
+S3_BUCKET_PATH_VMWARE="s3://box-cutter-us-east-2-cloudtrail/windows/vmware${VMWARE_TOOLS_VERSION}/"
 cmd="aws s3 sync ${DIR}/box/vmware/ ${S3_BUCKET_PATH_VMWARE} ${S3_GRANTS}"
 echo ${cmd}
 ${cmd}
-S3_BUCKET_PATH_VIRTUALBOX="s3://box-cutter-us-east-2-cloudtrail/virtualbox${VBOX_GUEST_ADDITIONS_VERSION}/"
+S3_BUCKET_PATH_VIRTUALBOX="s3://box-cutter-us-east-2-cloudtrail/windows/virtualbox${VBOX_GUEST_ADDITIONS_VERSION}/"
 cmd="aws s3 sync ${DIR}/box/virtualbox/ ${S3_BUCKET_PATH_VIRTUALBOX} ${S3_GRANTS}"
 echo ${cmd}
 ${cmd}
