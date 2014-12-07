82b1e7a2fdf7a684718b29861faee2f52d5dbafd
Mischa Taylor <mischa@misheska.com>
2014-12-06 18:33:51 -0800

    Retire old PowerShell-based script that disables Windows Update

--- floppy/rundisablewinupdate.cmd
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-SET ThisScriptsDirectory=%~dp0
-SET PowerShellScriptPath=%ThisScriptsDirectory%disablewinupdate.ps1
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%'";
