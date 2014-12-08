f438217234a0020a61499c0977d604c705e5db86
Mischa Taylor <mischa@misheska.com>
2014-12-08 01:27:23 -0800

    Disable updates on Windows 2008 R2 and Windows 7

--- floppy/disablewinupdate.bat
+++ floppy/disablewinupdate.bat
@@ -16,4 +16,15 @@ exit /b
 <job><script language="VBScript">
   Set ServiceManager = CreateObject("Microsoft.Update.ServiceManager")
   Set NewUpdateService = ServiceManager.AddService2("7971f918-a847-4430-9279-4a52d1efe18d",7,"")
+
+  Const NOT_CONFIGURED = 0
+  Const DISABLED = 1
+  Const PROMPT_TO_APPROVE_BEFORE_DOWNLOAD = 2
+  Const DOWNLOAD_AUTOMATICALLY = 3
+  Const SCHEDULED_INSTALLATION = 4
+  Set AutoUpdate = CreateObject("Microsoft.Update.AutoUpdate")
+  Set AutoUpdateSettings = AutoUpdate.Settings
+  AutoUpdateSettings.NotificationLevel = DISABLED
+  AutoUpdateSettings.Save
+  AutoUpdateSettings.Refresh
 </script></job>
