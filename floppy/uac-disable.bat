ff3783d3f4c7315fb25497acd3d3d347dc38d360
Mischa Taylor <mischa@misheska.com>
2015-01-11 23:40:48 -0800

    Add scripts to manage UAC settings

---
+++ floppy/uac-disable.bat
@@ -0,0 +1,18 @@
+@setlocal EnableDelayedExpansion EnableExtensions
+@for %%i in (%~dp0\_packer_config*.cmd) do @call "%%~i"
+@if defined PACKER_DEBUG (@echo on) else (@echo off)
+
+title Disabling UAC. Please wait...
+
+echo ==^> Disabling UAC
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /f /v EnableLUA /t REG_DWORD /d 0
+echo Reboot required to make this change effective.
+
+:exit0
+ver>nul
+goto :exit
+
+:exit1
+verify other 2>nul
+
+:exit
