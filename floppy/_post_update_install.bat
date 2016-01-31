3de57118a95afbacdd461adc0300a97ab6b4c5b8
jcoutch <jcoutch@users.noreply.github.com>
2016-01-31 13:24:14 -0500

    Added doco to post-update script

--- floppy/_post_update_install.bat
+++ floppy/_post_update_install.bat
@@ -1,1 +1,5 @@
+ECHO OFF
+REM This script is not included on the floppy by default.
+REM If you're using update.bat, include this file if you
+REM want to run scripts following all updates/reboots.
 a:\zz-start-sshd.cmd
