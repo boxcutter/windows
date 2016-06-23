309315ab862bdaf246bcf0bb015e3ead87bc3a97
Morrie Winnett <moe.winnett@gmail.com>
2016-06-23 09:31:51 -0700

    Add call to post update script

--- floppy/_post_update_install.bat
+++ floppy/_post_update_install.bat
@@ -1,5 +1,23 @@
-ECHO OFF
+@setlocal EnableDelayedExpansion EnableExtensions
+@for %%i in (%~dp0\_packer_config*.cmd) do @call "%%~i"
+@if defined PACKER_DEBUG (@echo on) else (@echo off)
+
 REM This script is not included on the floppy by default.
 REM If you're using update.bat, include this file if you
 REM want to run scripts following all updates/reboots.
-a:\zz-start-sshd.cmd
+
+title Running post update scripts. Please wait...
+
+@for %%i in (%~dp0\zz*.cmd) do (
+  echo ==^> Running "%%~i"  
+  @call "%%~i"
+)
+
+:exit0
+ver>nul
+goto :exit
+
+:exit1
+verify other 2>nul
+
+:exit
