02b9d49209710acdf2a8c9811a0aafb8e2ebed49
Tim Smith <tsmith@chef.io>
2018-05-01 23:33:10 -0700

    Rework disabling windows updates and fix bad templates
    Entirely rework and simplify how we disable windows update. This should
    be complete this time according to various docs I can find on the
    Iternet. Later versions of windows pretty aggressively reenable things.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- floppy/disablewinupdate.bat
+++ floppy/disablewinupdate.bat
@@ -3,31 +3,22 @@
 @for %%i in (%~dp0\_packer_config*.cmd) do @call "%%~i"
 @if defined PACKER_DEBUG (@echo on) else (@echo off)
 
-echo ==^> Enabling updates for other products from Microsoft Update
-net stop wuauserv
+echo ==^> Disabling Windows Updates
 
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v EnableFeaturedSoftware /t REG_DWORD /d 1 /f
+:: stop the service and disable it
+SC stop wuauserv
+SC config wuauserv start= disabled
 
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v IncludeRecommendedUpdates /t REG_DWORD /d 1 /f
+:: Notify before download
+reg add reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v AUOptions /t REG_DWORD /d 2 /f
 
-cscript //nologo "%~f0?.wsf"
+:: turn the whole thing off
+reg add "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate" /v DisableWindowsUpdateAccess /t REG_DWORD /d 1 /f
+reg add "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" /v NoAutoUpdate /t REG_DWORD /d 1 /f
 
-net start wuauserv
+:: disable the update prompt scheduled tasks
+schtasks.exe /change /tn "\Microsoft\Windows\UpdateOrchestrator\USO_UxBroker_Display" /disable
+schtasks.exe /change /tn "\Microsoft\Windows\UpdateOrchestrator\MusUx_UpdateInterval" /disable
+schtasks.exe /change /tn "\Microsoft\Windows\WindowsUpdate\sih" /disable
+
 exit /b
-
------ Begin wsf script --->
-<job><script language="VBScript">
-  Set ServiceManager = CreateObject("Microsoft.Update.ServiceManager")
-  Set NewUpdateService = ServiceManager.AddService2("7971f918-a847-4430-9279-4a52d1efe18d",7,"")
-
-  Const NOT_CONFIGURED = 0
-  Const DISABLED = 1
-  Const PROMPT_TO_APPROVE_BEFORE_DOWNLOAD = 2
-  Const DOWNLOAD_AUTOMATICALLY = 3
-  Const SCHEDULED_INSTALLATION = 4
-  Set AutoUpdate = CreateObject("Microsoft.Update.AutoUpdate")
-  Set AutoUpdateSettings = AutoUpdate.Settings
-  AutoUpdateSettings.NotificationLevel = DISABLED
-  AutoUpdateSettings.Save
-  AutoUpdateSettings.Refresh
-</script></job>
