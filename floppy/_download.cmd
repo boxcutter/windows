71013de0215412936be0626bcb1f9391c05ac27e
Dax T. Games <dax.games@bedbath.com>
2020-02-20 09:17:46 -0500

    disable bits

--- floppy/_download.cmd
+++ floppy/_download.cmd
@@ -58,6 +58,10 @@ powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url%', '%f
 
 if not errorlevel 1 if exist "%filename%" goto exit0
 
+if defined DISABLE_BITS (
+    if "%DISABLE_BITS%" == "1" if not exist "%filename%" goto exit1
+)
+
 :bitsadmin
 
 set bitsadmin=
